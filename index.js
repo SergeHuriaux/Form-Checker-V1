177a7b6895fe3e24ff3c629d2fcb975b3b628fc3
SergeHuriaux <sergeoclockpromox@gmail.com>
2021-12-15 12:37:20 -0400

    Création de fonction errorDisplay, email et pseudochecker

---
+++ index.js
@@ -0,0 +1,79 @@
+// Sélection de tous les inputs de notre form
+const inputs = document.querySelectorAll('input[type="text"], input[type="password"]');
+//console.log(inputs);
+
+// Variables pour stocker les entrées
+let pseudo, email, password, confirmPass;
+
+// Function erreur Display
+const errorDisplay = (tag, message, valid) => {
+    const container = document.querySelector("." + tag + "-container");
+    const span = document.querySelector("." + tag + "-container > span");
+    
+    if (!valid) {
+        container.classList.add('error');
+        span.textContent = message;
+    } else {
+        container.classList.remove('error');
+        span.textContent = message;
+    }
+}
+
+// Déclaration des fonctions checkers des 4 inputs
+const pseudoChecker = (value) => {
+    //console.log(value);
+    if (value.length > 0 && (value.length < 3 || value.length > 20)) {
+        errorDisplay("pseudo", "Le pseudo doit faire entre 3 et 20 caractères");
+        pseudo = null;
+    } else if (!value.match(/^[a-zA-Z0-9_.-]*$/)) {
+        errorDisplay("pseudo", "Le pseudo ne doiyt pas contenir de caractères spéciaux");
+        pseudo = null;
+    } else {
+        errorDisplay("pseudo", "", true);
+        pseudo = value;
+    }
+    
+}
+
+const emailChecker = (value) => {
+    if (!value.match(/^[\w_-]+@[\w-]+\.[a-z]{2,4}$/i)) {
+        errorDisplay("email", "Le mail que vous avez entré, n'est pas valide");
+        email = null;
+    } else {
+        errorDisplay('email', "", true);
+        email = value;
+    }        
+};
+
+const passwordChecker = (value) => {
+    console.log(value);
+};
+
+const confirmChecker = (value) => {
+    console.log(value);
+};
+
+
+
+// Pour écouter tous les inputs et ne pas les faires les uns après les autres
+inputs.forEach((input) => {
+    input.addEventListener("input", (e) => {
+        //console.log(e.target.id);
+        // Voir dans quel input se trouve le user afin de lancer la fonction correspondante
+        switch (e.target.id) {
+            case "pseudo":
+                pseudoChecker(e.target.value);
+                break;
+            case "email":
+                emailChecker(e.target.value);
+                break;
+            case "password":
+                passwordChecker(e.target.value);
+                break;
+            case "confirm":
+                confirmChecker(e.target.value);
+                break;
+            default: null;
+        }
+    });
+});
